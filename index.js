2e280d84c73ada210d9cebbe4a09b1e4d5cb5c11
jabaringure@gmail.com <jabaringure@gmail.com>
2024-10-17 15:42:13 +0300

    Updates Recipe Page

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const baseUrl = 'http://localhost:3000/Foods';
-
 function getFoods() {
     return fetch(baseUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        .catch(function() {
-            alert('Error! Failed to connect API.');
+        .then(response => response.json())
+        .catch(() => {
+            alert('Error! Failed to connect to API.');
             return [];
         });
 }
+
 function showFood(foodId) {
     getFoods().then(foodData => {
         const selectedFood = foodData.find(food => food.id == foodId);
@@ -41,8 +39,56 @@ function renderFoodPopup(food) {
 }
 
 const closeButton = document.querySelector(".close-btn");
-
 closeButton.onclick = function() {
     const popup = document.getElementById("foodPopup");
     popup.classList.remove("show");
+};
+
+const recipeForm = document.getElementById("recipeForm");
+recipeForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const foodName = document.getElementById("foodName").value;
+    const recipeDirections = document.getElementById("recipeDirections").value;
+    const ingredients = document.getElementById("ingredients").value.split(',');
+    const imgURL = document.getElementById("imageUrl").value
+    const newRecipe = {
+        title: foodName,
+        directions: recipeDirections,
+        img_URL: imgURL,
+        "ingredients 1": ingredients[0],
+        "ingredients 2": ingredients[1],
+        "ingredients 3": ingredients[2]
+    };
+
+    fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newRecipe)
+    })
+    .then(response => response.json())
+    .then(addedRecipe => {
+        addRecipeButton(addedRecipe);
+    })
+    recipeForm.reset();
+});
+
+function addRecipeButton(recipe) {
+    const searchBarRow = document.querySelector(".row");
+    const newButton = document.createElement("button");
+    newButton.innerHTML = `
+        ${recipe.title} 
+        <span class="delete-btn">X</span>
+    `;
+    newButton.onclick = function() {
+        showFood(recipe.id);
+    };
+    newButton.classList.add("recipe-button");
+    const deleteBtn = newButton.querySelector(".delete-btn");
+    deleteBtn.onclick = function(event) {
+        event.stopPropagation();
+        searchBarRow.removeChild(newButton);
+    };
+    searchBarRow.appendChild(newButton);
 }
